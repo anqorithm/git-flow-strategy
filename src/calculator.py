ad26e2da529f2d89e3011197651f6cb8f02d38a5
Abdullah Alqahtani <anqorithm@protonmail.com>
2025-07-18 21:42:33 +0300

    fix: handle negative input in the subtract function

--- src/calculator.py
+++ src/calculator.py
@@ -3,7 +3,7 @@ class Calculator:
         return a + b
     
     def subtract(self, a, b):
-        return a - b
+        return a - b if a > b else b - a
 
     def multiply(self, a, b):
         return a * b
